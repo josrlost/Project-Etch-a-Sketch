078e4c48743877785b91288e8cd43d4d8f6a79d2
josrlost <197001265+josrlost@users.noreply.github.com.>
2026-07-21 21:32:27 +0000

    Corrected the logic of the input validation for the user. Centered and added header to the html file, and modified css sheet

--- js/script.js
+++ js/script.js
@@ -9,23 +9,35 @@ expandGridBtn.addEventListener('click', () => creatingNewGrid());
 
 function creatingNewGrid() {
     limitLoop = prompt('Add new number of squares per side', 16);
-    if(limitLoop > 50) {prompt('Enter a number not greater than 50')}
-    else if(limitLoop <= 50) {
-    gridArray.forEach((ele) => ele.remove());
-    limitLoop = limitLoop * limitLoop;
-    for(i = 0; i < limitLoop; i++) {
-    gridArray.push(document.createElement('div'));
-    gridArray[i].classList.add('grid');
-    gridArray[i].textContent = '1';
-    mainContainer.appendChild(gridArray[i]);
-    console.log(gridArray);
-}; }
+    if(limitLoop > 80) {limitLoop = prompt('Enter a number not greater than 50');
+        gridArray.forEach((ele) => ele.remove());
+        limitLoop = limitLoop * limitLoop;
+        for(i = 0; i < limitLoop; i++) {
+        gridArray.push(document.createElement('div'));
+        gridArray[i].classList.add('grid');
+        gridArray[i].textContent = '1';
+        mainContainer.appendChild(gridArray[i]);
+        console.log(gridArray);  
+    } 
+}
+    else if(limitLoop <= 80) {
+        gridArray.forEach((ele) => ele.remove());
+        limitLoop = limitLoop * limitLoop;
+        for(i = 0; i < limitLoop; i++) {
+        gridArray.push(document.createElement('div'));
+        gridArray[i].classList.add('grid');
+        gridArray[i].textContent = '1';
+        mainContainer.appendChild(gridArray[i]);
+        console.log(gridArray);
+}; } gridArray.forEach((ele) => ele.addEventListener('mouseover', (evt) => {evt.target.style.backgroundColor = 'black'}));
+    gridArray.forEach((ele) => ele.addEventListener('mouseout', (evt) => {evt.target.style.backgroundColor = 'red'}));
 } 
 
 for(i = 0; i < limitLoop; i++) {
     gridArray.push(document.createElement('div'));
     gridArray[i].classList.add('grid');
     gridArray[i].textContent = '1';
+    gridArray[i].style.borderColor = 'black';
     mainContainer.appendChild(gridArray[i]);
     console.log(gridArray);
 };
