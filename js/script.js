78c5c8fa18ef58ef3708e98417dd9096dd4ad60c
josrlost <197001265+josrlost@users.noreply.github.com.>
2026-07-21 20:59:38 +0000

    Add function that first removes the loop that created the initial grid at the start of the page; and initializes a new loop that creates a series of divs and appends them to the parent div,  based on the user's input.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,31 @@
 const mainContainer = document.querySelector('#container');
+const expandGridBtn = document.querySelector('button');
 
+let limitLoop = 256;
 let gridArray = [];
 
-for(i = 0; i < 256; i++) {
+
+expandGridBtn.addEventListener('click', () => creatingNewGrid());
+
+function creatingNewGrid() {
+    limitLoop = prompt('Add new number of squares per side for the grid', 256);
+    gridArray.forEach((ele) => ele.remove());
+    limitLoop = limitLoop * limitLoop;
+    for(i = 0; i < limitLoop; i++) {
     gridArray.push(document.createElement('div'));
     gridArray[i].classList.add('grid');
     gridArray[i].textContent = '1';
     mainContainer.appendChild(gridArray[i]);
     console.log(gridArray);
 };
+} 
 
+for(i = 0; i < limitLoop; i++) {
+    gridArray.push(document.createElement('div'));
+    gridArray[i].classList.add('grid');
+    gridArray[i].textContent = '1';
+    mainContainer.appendChild(gridArray[i]);
+    console.log(gridArray);
+};
 
 gridArray.forEach((ele) => ele.addEventListener('mouseover', (evt) => {evt.target.style.backgroundColor = 'black'}));
