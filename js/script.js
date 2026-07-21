23bf7423a6f699be409864d9f40a252c2f89c280
josrlost <197001265+josrlost@users.noreply.github.com.>
2026-07-21 22:36:11 +0000

    Compelted the first requirement of the TOP extra credit, by randomizing each color trigger by the mouseover and mouseout events, randomizing rgb values with template literals

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 const mainContainer = document.querySelector('#container');
 const expandGridBtn = document.querySelector('button');
 
-let limitLoop = 256;
+let limitLoop = 522;
 let gridArray = [];
 
+function randomizeColor(num) {
+   return Math.floor(Math.random() * num);
+}
 
 expandGridBtn.addEventListener('click', () => creatingNewGrid());
 
@@ -15,7 +18,10 @@ function creatingNewGrid() {
         for(i = 0; i < limitLoop; i++) {
         gridArray.push(document.createElement('div'));
         gridArray[i].classList.add('grid');
-        gridArray[i].textContent = '1';
+        gridArray[i].textContent = ' ';
+        gridArray[i].style.blockSize = "72px";
+        gridArray[i].style.inlineSize = "72px";
+        gridArray[i].style.border = '2px solid black';
         mainContainer.appendChild(gridArray[i]);
         console.log(gridArray);  
     } 
@@ -26,7 +32,10 @@ function creatingNewGrid() {
         for(i = 0; i < limitLoop; i++) {
         gridArray.push(document.createElement('div'));
         gridArray[i].classList.add('grid');
-        gridArray[i].textContent = '1';
+        gridArray[i].textContent = ' ';
+        gridArray[i].style.blockSize = "72px";
+        gridArray[i].style.inlineSize = "72px";
+        gridArray[i].style.border = '2px solid black';
         mainContainer.appendChild(gridArray[i]);
         console.log(gridArray);
 }; } gridArray.forEach((ele) => ele.addEventListener('mouseover', (evt) => {evt.target.style.backgroundColor = 'black'}));
@@ -36,11 +45,13 @@ function creatingNewGrid() {
 for(i = 0; i < limitLoop; i++) {
     gridArray.push(document.createElement('div'));
     gridArray[i].classList.add('grid');
-    gridArray[i].textContent = '1';
+    gridArray[i].textContent = ' ';
+    gridArray[i].style.blockSize = "97px";
+    gridArray[i].style.inlineSize = "97px";
     gridArray[i].style.border = '2px solid black';
     mainContainer.appendChild(gridArray[i]);
     console.log(gridArray);
 };
 
-gridArray.forEach((ele) => ele.addEventListener('mouseover', (evt) => {evt.target.style.backgroundColor = 'black'}));
-gridArray.forEach((ele) => ele.addEventListener('mouseout', (evt) => {evt.target.style.backgroundColor = 'red'}));
+gridArray.forEach((ele) => ele.addEventListener('mouseover', (evt) => {evt.target.style.backgroundColor = `rgb(${randomizeColor(255)} ${randomizeColor(255)} ${randomizeColor(255)}`}));
+gridArray.forEach((ele) => ele.addEventListener('mouseout', (evt) => {evt.target.style.backgroundColor = `rgb(${randomizeColor(255)} ${randomizeColor(255)} ${randomizeColor(255)}`}));
