803431d7abb9fb8be1a60409221732dd6f803708
josrlost <197001265+josrlost@users.noreply.github.com.>
2026-07-21 20:14:22 +0000

    Add event mouseover to change background effect on each div using the forEach method within the div containing array object. Added some text to help visualize the grid

--- js/script.js
+++ js/script.js
@@ -2,10 +2,13 @@ const mainContainer = document.querySelector('#container');
 
 let gridArray = [];
 
-for(i = 0; i <= 256; i++) {
+for(i = 0; i < 256; i++) {
     gridArray.push(document.createElement('div'));
+    gridArray[i].classList.add('grid');
+    gridArray[i].textContent = '1';
     mainContainer.appendChild(gridArray[i]);
     console.log(gridArray);
 };
 
 
+gridArray.forEach((ele) => ele.addEventListener('mouseover', (evt) => {evt.target.style.backgroundColor = 'black'}));
